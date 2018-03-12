b281821a5c3b6a1bffa825342abd3cf44428dd15
nishla <nishla@rumsan.com>
2018-03-12 15:07:24 +0545

    Save local changes

--- src/modules/Header/index.js
+++ src/modules/Header/index.js
@@ -24,7 +24,7 @@ class Header extends Component {
 				<div className="container-fluid">
 					<div className="navbar-header">
 
-						<Link to={'/'} className="navbar-brand"><img src="/images/favicon.ico" width="18"/><b>Hacker News </b></Link>
+						<Link to={'/'} className="navbar-brand"><img src={process.env.PUBLIC_URL + '/'} width="18"/><b>Hacker News </b></Link>
 						<span className="navbar-link">
 								<Link to={'#'}>New </Link>&nbsp;|&nbsp;
 								<Link to={'#'}>Comments</Link>&nbsp;|&nbsp;
